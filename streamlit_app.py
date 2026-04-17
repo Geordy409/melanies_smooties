27e71c0c0e887bcb79ef5a2b220ba24c58b5272b
Geordy409 <116566307+Geordy409@users.noreply.github.com>
2026-04-17 10:50:10 +0200

    Remove Smoothiefroot API call and dataframe
    Removed API call to Smoothiefroot and related dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,9 +58,6 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
       
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-) 
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
     
